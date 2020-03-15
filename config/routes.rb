ed26f807c4f682af8db009196fb8a7cfa4864c2b
Glenn <glenn.marcano90@gmail.com>
2020-03-15 15:56:52 -0300

    sign in and sign out added, works!

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :communications
   resources :buildings
 
-  root to: "buildings#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

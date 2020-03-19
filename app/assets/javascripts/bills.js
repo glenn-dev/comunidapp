23a9f02d5048cf7e960a3e7fed2907b198f450b9
Glenn <glenn.marcano90@gmail.com>
2020-03-19 11:46:49 -0300

    Bill form worksgit add .

--- app/assets/javascripts/bills.js
+++ app/assets/javascripts/bills.js
@@ -1,7 +1,14 @@
 $(document).on('ready turbolinks:load ajax:complete', function(){
 	$('#add-expense').click(function(e){
 		e.preventDefault()
-		var addExpense = $(this).data('expense')
+		var addExpense = $($(this).data('expense'))
+		addExpense.find("input, select, textarea").each(function() {
+			$(this).attr("name", function() {
+				return $(this)
+					.attr("name")
+					.replace("new_record", new Date().getTime());
+			});
+		});
 		$('.expenses_detail_td').append(addExpense)
 	});
 });
--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,6 +1,6 @@
 module BillsHelper
     def add_expenses_detail_button(form)
-		  expenses_fields = form.fields_for(:expenses_details, ExpensesDetail.new) do |expenses_form|
+		  expenses_fields = form.fields_for(:expenses_details, ExpensesDetail.new, child_index: "new_record") do |expenses_form|
 		  	render 'bills/expenses/expenses_fields', expenses_form: expenses_form
 		  end
       html_options = {

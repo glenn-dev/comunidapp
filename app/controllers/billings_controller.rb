55c8425235513e1bcd01cbb899901200e008a7e8
Glenn <glenn.marcano90@gmail.com>
2020-03-19 22:52:11 -0300

    heroku host added to paypal payment controller

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -17,8 +17,8 @@ class BillingsController < ApplicationController
         payer: {
         payment_method: "paypal" },
         redirect_urls: {
-        return_url: "http://localhost:3000/billings/execute",
-        cancel_url: "http://localhost:3000/" },
+        return_url: "https://afternoon-island-22684.herokuapp.com/billings/execute",
+        cancel_url: "https://afternoon-island-22684.herokuapp.com/" },
         transactions: [{
           :item_list => {
             :items => [{
